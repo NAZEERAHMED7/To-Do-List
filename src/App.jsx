8d30a064139c8b1a7c48a50f9a437867737df097
NAZEERAHMED7 <nazeerbossyt777@gmail.com>
2024-10-30 00:17:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Task from "./components/to-do-input"
+function App() {
+
+  return (
+      <div className="max-w-[80vw] h-screen px-4 py-10 mx-auto bg-slate-900 flex items-center">
+        <Task/>
+      </div>
+  )
+}
+
+export default App
